223d74776429f8a9229a752cb2dcdef5fdacd94f
Owen Jones <owen@owenjones.net>
2013-06-25 02:03:07 +0100

    Removed testing code and started putting together the different layers

--- __main__.py
+++ __main__.py
@@ -1,11 +1,6 @@
-"""
-Ignore the stuff that goes on in this file, it's kind of my testing
-and building area at the minute.
-"""
-import json
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import mandelbot
 
 if __name__ == '__main__' :
-
-    with open("config.json") as fp:
-        conf = json.load(fp)
-        print(conf)
+    mandelbot.run()
