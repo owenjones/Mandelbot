28c858c88622e54d25639fdbb7166f53bd8a5b3e
Owen Jones <owen@owenjones.net>
2013-06-27 00:22:21 +0100

    Clarified some things and tided the configuration methods

--- mandelbot/utils.py
+++ mandelbot/utils.py
@@ -2,7 +2,7 @@
 """
 Provides basic utilties for running Mandelbot
 """
-import time, json, base64
+import time, base64, json
 
 def console(message) :
     print("[{}] {}".format(logtime(), message))
@@ -15,8 +15,8 @@ def logtime(t = None) :
 Password Methods
 Handle encoding/decoding network and user passwords for slightly safer storage
 
-encode (str) - Returns an encoded string
-decode (str) - Returns a decoded password
+encode (str) - Returns an encoded password as a string
+decode (str) - Returns a decoded password as a string
 """
 class password(object) :
 
@@ -30,7 +30,10 @@ class password(object) :
 Configuration Methods
 These deal with loading and saving configuration files
 
-save (dict) - Replaces the current configuration file with an updated one
+build - Takes a list of network and module objects, strips the configurations
+      - from each and packages them in a dictionary to be stored
+
+save  - Replaces the current configuration file with an updated one
 """
 class config(object) :
     file = None
@@ -45,10 +48,35 @@ class config(object) :
     def __getattr__(self, name) :
         return self.loaded[name]
 
+    def build(self, networks = [], modules = []) :
+        conf = {}
+        nets = []
+        mods = []
+
+        for n in networks :
+            nets.append(n.config)
+
+        for m in modules :
+            mods.append(m.config)
+
+        if nets :
+            conf["networks"] = nets
+
+        if mods :
+            conf["modules"] = mods
+
+        self.save(conf)
+
+
     def save(self, new) :
         try :
             assert(isinstance(new, dict))
             assert(isinstance(new["networks"], list))
+
+            assert(len(new["networks"]) > 0)
+
+            if new["modules"] :
+                assert(isinstance(new["modules"], list))
 
             with open(self.file, "w") as fp :
                 json.dump(new, fp)
@@ -57,7 +85,7 @@ class config(object) :
             utils.console("Updating configuration failed - invalid data structure")
 
         except Exception as e :
-            utils.console("Updating configuration failed - {}".format(e.strerror))
+            utils.console("Updating configuration failed - {}".format(str(e)))
 
 
 class message(object) :
