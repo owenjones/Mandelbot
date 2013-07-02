a0bfdb0b56791070b3f0df40efb519b24800c1ab
Owen Jones <owen@owenjones.net>
2013-07-03 00:39:54 +0100

    Can now handle PINGs properly, working on other messages

--- mandelbot/parser.py
+++ mandelbot/parser.py
@@ -4,6 +4,7 @@ Message Parsing
 Provides methods to parse incoming messages from an IRC network, and to
 register callbacks for commands.
 """
+import re
 from . import utils
 
 class parser(object) :
@@ -15,34 +16,34 @@ class parser(object) :
         self.network = network
 
         self.builtin = {"PING"    : (self.network, "pong"),
-                        "NOTICE"  : (self, "_notice"),
-                        "PRIVMSG" : (self, "_privmsg")}
+                        "NOTICE"  : (self, "message"),
+                        "PRIVMSG" : (self, "message"),
+                        "KICK"    : (self.network, "kicked"),
+                        "NICK"    : (self.network, "nickchange"),
+                        "ERROR"   : (self, "error")}
+
+    def host(self, line) :
+        return False
 
     def call(self, call, param) :
         try :
             call = self.builtin[call]
             print("Calling {}".format(call))
-
-        except KeyError :
-            try :
-                call = self.callbacks[call]
-
-            except KeyError :
-                utils.console("Callback {} not found".format(call))
-
-        try :
             getattr(call[0], call[1])(param)
 
-        except Exception as e :
-            utils.console(str(e))
+        except KeyError :
+            pass
 
     def parse(self, raw) :
         try :
-            a, b = raw.split(" :")
-            parsed = {}
+            print(raw)
+            test = raw.split(" ", 1)
+            if test[0] in ("PING", "ERROR") :
+                self.call(test[0], test[1])
+                return
 
-            if a.strip() == "PING" :
-                self.call("PING", b)
+            addr, cmd, tgt, msg = raw.split(" ", 3)
+
 
         except Exception as e :
             utils.console(str(e))
