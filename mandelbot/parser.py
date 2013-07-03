11586d84833f61365f038b8188769e55274739e7
Owen Jones <owen@owenjones.net>
2013-07-04 00:48:44 +0100

    Can now parse messages, for the most part

--- mandelbot/parser.py
+++ mandelbot/parser.py
@@ -4,46 +4,83 @@ Message Parsing
 Provides methods to parse incoming messages from an IRC network, and to
 register callbacks for commands.
 """
-import re
 from . import utils
 
 class parser(object) :
     network = None
     builtin = {}
-    callbacks = {}
+    callback = {}
 
     def __init__(self, network) :
         self.network = network
 
         self.builtin = {"PING"    : (self.network, "pong"),
+                        "001"     : (self.network, "connected"),
+                        "JOIN"    : (self.network, "joined"),
+                        "NICK"    : (self.network, "nickchange"),
+                        "MODE"    : (self.network, "modechange"),
+                        "ERROR"   : (self, "error"),
                         "NOTICE"  : (self, "message"),
                         "PRIVMSG" : (self, "message"),
-                        "KICK"    : (self.network, "kicked"),
-                        "NICK"    : (self.network, "nickchange"),
-                        "ERROR"   : (self, "error")}
+                        "KICK"    : (self.network, "kicked")}
 
-    def host(self, line) :
-        return False
+        self.callback = {"quit" : (self.network, "quit")}
 
-    def call(self, call, param) :
+    def host(self, addr) :
+        addr = addr[1:]
+        nsplit = addr.find("!")
+        hsplit = addr.find("@")
+        nick = addr[:nsplit] if (nsplit > 0) else addr
+        user = addr[(nsplit + 1):hsplit] if (nsplit > 0) else False
+        host = addr[(hsplit + 1):] if (hsplit > 0) else addr
+
+        return {"nick": nick, "user": user, "host": host}
+
+    def callBuiltin(self, call, params) :
         try :
             call = self.builtin[call]
-            print("Calling {}".format(call))
-            getattr(call[0], call[1])(param)
+            getattr(call[0], call[1])(params)
 
-        except KeyError :
+        except (KeyError, AttributeError) :
             pass
 
-    def parse(self, raw) :
+    def callCallback(self, cmd, params) :
         try :
-            print(raw)
-            test = raw.split(" ", 1)
-            if test[0] in ("PING", "ERROR") :
-                self.call(test[0], test[1])
-                return
+            call = self.callback[cmd]
+            getattr(call[0], call[1])(params)
 
-            addr, cmd, tgt, msg = raw.split(" ", 3)
+        except KeyError :
+            self.network.message(params[1][2], "Command not registered")
 
+    def parse(self, raw) :
+        raw = raw.strip()
+        print(raw)
+        parts = raw.split(" ", 3)
+        if parts[0] == "PING" :
+            self.callBuiltin("PING", parts[1])
 
-        except Exception as e :
-            utils.console(str(e))
+        elif parts[1] in ("JOIN", "NICK", "MODE", "ERROR") :
+            addr = self.host(parts[0])
+            self.callBuiltin(parts[1], (addr, parts[2]))
+
+        else :
+            addr, cmd, tgt, msg = parts
+            addr = self.host(addr)
+
+            if tgt[0] != "#" :
+                tgt = addr["nick"]
+
+            self.callBuiltin(cmd, (addr, cmd, tgt, msg))
+
+    def message(self, params) :
+        msg = params[3][1:]
+
+        if msg[0] == self.network.config["command"] :
+            parts = msg.split(" ", 1)
+
+            print(parts)
+
+            cmd = parts[0][1:]
+            flags = parts[1] if (len(parts) > 1) else False
+
+            self.callCallback(cmd, (flags, params))
