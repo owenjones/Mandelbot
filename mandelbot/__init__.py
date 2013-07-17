4d130573c29f0ca29e8d77068da9cef4d3967086
Owen Jones <owen@owenjones.net>
2013-07-17 23:49:04 +0100

    Added command registering/triggering

--- mandelbot/__init__.py
+++ mandelbot/__init__.py
@@ -5,8 +5,7 @@ __author__ = "Owen Jones"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2013 Owen Jones"
 
-from . import utils, network
-from .exceptions import *
+from mandelbot import utils, network, features
 
 def run(config = None) :
     utils.console("{} {} is launching..".format(__title__, __version__))
@@ -14,16 +13,15 @@ def run(config = None) :
 
 class Mandelbot(object) :
     config = None
-    modules = []
     networks = []
+    commands = {}
 
     def __init__(self, conf = None) :
         conf = conf if conf else "config.json"
         self.config = utils.config(conf)
         self.config.load()
-
         self.loadNetworks()
-        self.loadModules()
+        features.load(self)
 
     def loadNetworks(self) :
         for n in self.config.networks :
@@ -32,8 +30,21 @@ class Mandelbot(object) :
             if n["autoconnect"] :
                 net.connect()
 
-    def loadModules(self) :
-        pass
+    def registerCommand(self, command, call) :
+        self.commands[command] = call
+
+    def triggerCommand(self, net, command, params) :
+        try :
+            call = self.commands[command]
+
+        except KeyError :
+            net.reply("Command \"{}\" not registered".format(command), params)
+
+        try :
+            getattr(call[0], call[1])(net, params)
+
+        except Exception as e :
+            net.reply("Error with command: [{}]".format(e), params)
 
     def shutdown(self, message = None) :
         utils.console("Shutting down...")
@@ -42,4 +53,4 @@ class Mandelbot(object) :
             if n.state.isConnected :
                 n.quit(message)
 
-        self.config.build(self.networks, self.modules)
+        self.config.build(self.networks)
