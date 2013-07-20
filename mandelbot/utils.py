7352cff7c712acec981acfb8898bdb26c0e51fcf
Owen Jones <owen@owenjones.net>
2013-07-20 18:21:46 +0100

    Host string parsing added to utils

--- mandelbot/utils.py
+++ mandelbot/utils.py
@@ -5,42 +5,50 @@ Provides basic utilties for running Mandelbot
 import time, base64, json
 
 def console(message) :
-    print("[{}] {}".format(logtime(), message))
+    """Desplays a formatted message on the terminal"""
+    formatted = "[{}] {}".format(logtime(), message)
+    print(formatted)
 
 def logtime(t = None) :
+    """Returns the time in a log-friendly format"""
     t = t if t else time.time()
     return time.strftime("%H:%M:%S", time.localtime(t))
+
+def host(addr) :
+    """Splits a received host string into nickname, username and host"""
+    addr = addr[1:]
+    nsplit = addr.find("!")
+    hsplit = addr.find("@")
+    nick = addr[:nsplit] if (nsplit > 0) else addr
+    user = addr[(nsplit + 1):hsplit] if (nsplit > 0) else False
+    host = addr[(hsplit + 1):] if (hsplit > 0) else addr
+
+    return {"nick": nick, "user": user, "host": host}
 
 """
 Password Methods
 Handle encoding and decoding network and user passwords for slightly "safer" storage
 (Stops people from being able to straight-up read your password from the config)
-
-encode (str) - Returns an encoded password as a string
-decode (str) - Returns a decoded password as a string
 """
 class password(object) :
 
     def encode(p) :
+        """Returns an encoded password as a string"""
         return base64.b64encode(p.encode("UTF-8")).decode("UTF-8")
 
     def decode(p) :
+        """Returns a decoded password as a string"""
         return base64.b64decode(p.encode("UTF-8")).decode("UTF-8")
 
 """
 Configuration Methods
-
-__init__ (str) - Initiates the configuration session using the specified file
-load           - Loads the configuration from the configuration file
-build          - Takes a list of network objects, strips the configurations
-               - from each and packages them in a dictionary to be stored
-save           - Replaces the current configuration file with an updated one
 """
 class config(object) :
     file = None
     loaded = {}
 
     def __init__(self, file = None) :
+        """Initiates the configuration session using the specified file"""
         self.file = file
 
     def __getattr__(self, name) :
@@ -51,6 +59,7 @@ class config(object) :
             return False
 
     def load(self) :
+        """Loads the configuration from the configuration file"""
         try :
             with open(self.file, "r") as fp :
                 self.loaded = json.load(fp)
@@ -61,6 +70,8 @@ class config(object) :
             exit(console("Aborting Mandelbot launch..."))
 
     def build(self, networks = []) :
+        """Takes a list of network objects, strips the configurations
+        from each and packages them in a dictionary to be stored"""
         console("Building new configuration...")
 
         try :
@@ -85,6 +96,7 @@ class config(object) :
             console("Building configuration failed - {}".format(e))
 
     def _save(self, new) :
+        """Replaces the current configuration file with an updated one"""
         try :
             with open(self.file, "w") as fp :
                 json.dump(new, fp)
