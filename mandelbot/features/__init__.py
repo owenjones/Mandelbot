77d216929d7b40dc014abbcb675e9cc9e7b2a188
Owen Jones <owen@owenjones.net>
2013-07-20 18:18:21 +0100

    Exceptions from initalising features now caught and returned

--- mandelbot/features/__init__.py
+++ mandelbot/features/__init__.py
@@ -4,6 +4,7 @@ Mandelbot Features
 
 Imports all the Mandelbot features, and registers their callbacks with the bot
 """
+from mandelbot import utils
 import os
 
 features = [feature.split(".")[0] for feature in os.listdir(os.getcwd() + "/mandelbot/features")
@@ -11,6 +12,10 @@ features = [feature.split(".")[0] for feature in os.listdir(os.getcwd() + "/mand
 
 def load(bot) :
     for feature in features :
-        f = __import__("mandelbot.features." + feature, fromlist = [feature])
-        f.initalise(bot)
-        del f
+        try :
+            f = __import__("mandelbot.features." + feature, fromlist = [feature])
+            f.initalise(bot)
+            del f
+
+        except Exception as e :
+            utils.console("Error loading feature \"{}\" [{}]".format(feature, e))
