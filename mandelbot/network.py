cb0d62410e507c03ecef2d7308a58747a775f03b
Owen Jones <owen@owenjones.net>
2013-07-20 18:19:17 +0100

    Parsing of messages, and calling commands/events now handled by the network object

--- mandelbot/network.py
+++ mandelbot/network.py
@@ -6,78 +6,66 @@ Handles establishing a connection to, and interacting with, the IRC network.
 TODO:
 * Implement anti-flooding (buffer & time limit)
 * Separate basic commands from network model
-
-Properties:
-bot (object)               - the current running instance of Mandelbot
-connection (object)        - the established connection to the IRC server
-parser (object)            - the message parser object for this network
-state (object)             - the state object for this network
-delimiter (str)            - the character(s) that break up messages being sent to the network
-buff (list)                - a list of messages waiting to be sent to the IRC network (prevents flooding)
-messages (list)            - a list of messages received from the IRC network
-modes (list)               - the modes applied to the bot on this network
-
-config: name (str)         - the name of the network
-        host (str)         - the address of the IRC network server
-        port (int)         - the port the IRC server listens on
-        ssl (bool)         - whether to connect using SSL or not
-        password (str)     - the password for the IRC server (if required)
-        autoconnect (bool) - whether to automatically connect to the IRC server when Mandelbot is launched
-        nickserv (str)     - the identification service on this network
-        username (str)     - used to identify Mandelbot with the IRC server
-        nickpass (str)     - used to identify Mandelbot with the IRC server
-        realname (str)     - used to identify Mandelbot with the IRC server
-        nickname (str)     - used to identify Mandelbot with the IRC server
-        command (str)      - the command identifier Mandelbot listens for on this network
-        owner (str)        - the IRC user who can administrate Mandelbot on this network
-        users (list)       - a list of IRC users who can access Mandelbot's protected functions on this network
-        chans (dict)       - a dictionary of the channels Mandelbot is to join,
-                             and the key (if required)
-
-Methods:
-connect                    - attempts to create a connection to the IRC server
-identify                   - identifies Mandelbot with the IRC network
-send                       - sends a message to the specified channel or user
-notice                     - sends a notice to the specified channel or user
-join                       - joins a channel on the network
-part                       - leaves a channel on the network
-quit                       - leaves the IRC network and closes the connection to the server
 """
-from mandelbot import connection, utils, parser, channel
+from mandelbot import utils, connection, channel
 from mandelbot.exceptions import *
 
-class network(object) :
+"""
+Network States
+"""
+class state(object) :
+    isConnected = False
+    isWelcome = False
+    isIdentified = False
+
+    def connected(self) :
+        self.isConnected = True
+
+    def welcome(self) :
+        self.isWelcome = True
+
+    def identified(self) :
+        self.isIdentified = True
+
+class network(state) :
     bot = None
     connection = None
-    parser = None
-    state = None
     delimiter = "\r\n"
     buff = []
     messages = []
     modes = []
     channels = {}
-    config = {} # This will be populated when the network is loaded from the configuration
+    config = {}
+
+    events = {"PING"    : "_pong",
+              "001"     : "_connected",
+              "JOIN"    : "_joined",
+              "NICK"    : "_nick",
+              "MODE"    : "_mode",
+              "KICK"    : "_kicked",
+              "NOTICE"  : "_message",
+              "PRIVMSG" : "_message"}
 
     def __init__(self, config, bot) :
         self.bot = bot
         self.config = config
-        self.state = state()
 
-        self.parser = parser.parser(self)
-        self.parser.builtin = {"PING"    : (self, "pong"),
+    def event(self, call, params) :
+        """Triggers the handler for the network event"""
+        try :
+            call = self.events[call]
 
-                               # Trigger Network Events
-                               "001"     : (self, "connected"),
-                               "JOIN"    : (self, "joined"),
-                               "NICK"    : (self, "nickchanged"),
-                               "MODE"    : (self, "modechanged"),
-                               "KICK"    : (self, "kicked"),
+        except KeyError :
+            return
 
-                               # Handle received messages
-                               "NOTICE"  : (self.parser, "message"),
-                               "PRIVMSG" : (self.parser, "message")}
+        try :
+            getattr(self, call)(params)
+
+        except Exception as e :
+            utils.console("Error with builtin: [{}]".format(e))
 
     def connect(self) :
+        """Connects to the IRC network"""
         try :
             self.connection = connection.connection(self.config["host"],
                                                     self.config["port"],
@@ -86,7 +74,7 @@ class network(object) :
 
             self.connection.connect()
             utils.console("Connection established ({})".format(self.config["name"]))
-            self.connection.handler = (self.parser, "parse")
+            self.connection.handler = (self, "parse")
 
             if self.config["password"] :
                 self.send("PASS {}".format(utils.password.decode(self.config["password"])))
@@ -95,41 +83,67 @@ class network(object) :
             self.nick()
 
         except InvalidConnectionInformation :
-            raise InvalidServer
+            utils.console("Connection information for network \"{}\" is invalid.".format(self.config["name"]))
 
         except CouldNotConnect :
-            raise NoServerConnection
+            utils.console("Could not connect to network \"{}\".".format(self.config["name"]))
+
+    def parse(self, raw) :
+        """Parses a received message from the network"""
+        parts = raw.strip().split(" ", 3)
+        if parts[0] == "PING" :
+            self.event("PING", parts[1])
+
+        elif parts[1] in ("JOIN", "NICK", "PART") :
+            addr = utils.host(parts[0])
+            msg = parts[2][1:] if parts[2][0] == ":" else parts[2]
+            self.event(parts[1], (addr, msg))
+
+        else :
+            addr, cmd, tgt, msg = parts
+            addr = utils.host(addr)
+
+            if tgt[0] != "#" :
+                tgt = self.config["nickname"] if cmd == "MODE" else addr["nick"]
+
+            msg = msg[1:] if msg[0] is ":" else msg
+
+            self.event(cmd, (addr, cmd, tgt, msg))
 
     def send(self, message) :
+        """Sends a message to the network"""
         message = message + self.delimiter
         self.connection.send(message)
 
-    def join(self, chan, key) :
+    def join(self, chan, key = False) :
+        """Joins a channel on the network"""
         chan = chan.lower()
         self.channels[chan] = channel.channel(self, chan, key)
         self.channels[chan].join()
 
     def joinChannels(self) :
+        """Joins the channels saved in the network configuration"""
         for chan in self.config["chans"] :
             self.join(chan["name"], chan["key"])
 
-    def quit(self, message) :
+    def quit(self, message = False) :
+        """Gracefully closes the connection to this network"""
         q = "QUIT :{}".format(message) if message else "QUIT"
         self.send(q)
         self.connection.close(True)
 
     def shutdown(self, message) :
+        """Gracefully closes the connection to all networks, and halts the bot"""
         bot.shutdown(message)
-
-    def reply(self, message, params) :
-        self.message(params[1][2], message)
 
     # Nickname Managment
     def nick(self, nick = None) :
+        """Changes the bots nickname on the network"""
         nick = nick if nick else self.config["nickname"]
         self.send("NICK :{}".format(nick))
 
     def identify(self, params = None) :
+        """Identifies the bot on the network"""
         if self.config["nickpass"] :
             utils.console("Identifying as {} on {}...".format(self.config["nickname"],
                                                               self.config["name"]))
@@ -141,25 +155,46 @@ class network(object) :
             self.nick()
 
     # General Message Types
-    def pong(self, host) :
-        self.send("PONG :{}".format(host[1:]))
-
     def message(self, target, message) :
+        """Sends a message to a target on the network"""
         self.send("PRIVMSG {} :{}".format(target, message))
 
     def notice(self, target, message) :
+        """Sends a notice to a target on the network"""
         self.send("NOTICE {} :{}".format(target, message))
 
-    # Network Events
-    def connected(self, params) :
-        self.identify()
-        self.state.connected()
+    def reply(self, message, params) :
+        """Sends a message to the invoker of a command"""
+        self.message(params[1][2], message)
 
-    def nickchanged(self, params) :
+    # Network Events
+    def _pong(self, host) :
+        """PING"""
+        self.send("PONG :{}".format(host[1:]))
+
+    def _message(self, params) :
+        """When PRIVMSG and NOTICE are received"""
+        msg = params[3]
+
+        if msg[0] == self.config["command"] :
+            parts = msg.split(" ", 1)
+            cmd = parts[0][1:]
+            flags = parts[1] if (len(parts) > 1) else False
+
+            self.bot.triggerCommand(self, cmd, (flags, params))
+
+    def _connected(self, params) :
+        """When the bot has successfully connected"""
+        self.identify()
+        self.connected()
+
+    def _nick(self, params) :
+        """When a nickname is changed"""
         if params[0]["nick"] == self.config["nickname"] :
             self.config["nickname"] = params[1]
 
-    def modechanged(self, params) :
+    def _mode(self, params) :
+        """When a mode is changed"""
         if params[2] == self.config["nickname"] :
             modes = params[3]
             action = "append" if modes[0] == "+" else "remove"
@@ -167,44 +202,25 @@ class network(object) :
             for m in modes[1:] :
                 if m == "r" and action == "append" :
                     utils.console("...identified")
-                    self.state.identified()
+                    self.identified()
                     self.joinChannels()
 
                 getattr(self.modes, action)(m)
 
-    def joined(self, params) :
+    def _joined(self, params) :
+        """When somebody joins a channel"""
         if params[0]["nick"] == self.config["nickname"] :
             utils.console("{} joined {}".format(self.config["nickname"], params[1]))
             self.channels[params[1].lower()].joined()
         else :
             self.channels[params[1].lower()].users.append(params[2])
 
-    def names(self, params) :
-        pass
-
-    def kicked(self, params) :
+    def _kicked(self, params) :
+        """When somebody is kicked from a channel"""
         kicked = params[3].split(" ", 1)
         if kicked[0] == self.config["nickname"] :
-            utils.console("{} kicked from {} on {} ({})".format(self.config["nickname"], params[2], self.config["name"], kicked[1][1:]))
+            utils.console("{} kicked from {} on {} ({})".format(self.config["nickname"],
+                                                                params[2],
+                                                                self.config["name"],
+                                                                kicked[1][1:]))
             self.channels[params[2].lower()].join()
-
-"""
-Network States
-"""
-class state(object) :
-    isConnected = False
-    isWelcome = False
-    isIdentified = False
-    isActive = False
-
-    def connected(self) :
-        self.isConnected = True
-
-    def welcome(self) :
-        self.isWelcome = True
-
-    def identified(self) :
-        self.isIdentified = True
-
-    def active(self) :
-        self.isActive = True
