8f50d2b1a7d66a42764bf643f572d950aaa31dfe
Owen Jones <owen@owenjones.net>
2013-07-21 01:03:33 +0100

    Introduced timing thread to get the scores

--- mandelbot/features/howzat.py
+++ mandelbot/features/howzat.py
@@ -1,11 +1,31 @@
 # Mandelbot
 """
-Howzat! Cricket Scores
+Howzat! (Cricket Scores)
 """
 import sys
+from threading import Timer
+
+score = []
+thread = None
 
 def initalise(bot) :
+    global thread
     bot.registerCommand("howzat", (sys.modules[__name__], "howzat"))
+    #thread = Timer(120, "fetcher")
+
+def fetcher() :
+    global score, thread
 
 def howzat(obj, flags) :
-    pass
+    global score, thread
+    if flags and flags[0].lower in ("stop", "exit") :
+        scorethread.stop()
+        return
+
+    if score :
+        reply = score[-1:]
+
+    else :
+        "[Error] No Scores Recorded"
+
+    obj.reply(flags, reply)
