94c79b3f4566010ccf4a224c6b36291b409bd27e
Owen Jones <owen@owenjones.net>
2013-07-23 01:18:53 +0100

    Inners can now take extra parameters

--- mandelbot/decorators.py
+++ mandelbot/decorators.py
@@ -1,13 +1,13 @@
 #Mandelbot
 def owner(commandCall) :
-    def inner(obj, params) :
+    def inner(obj, params, *args) :
         if params[1][0].user == obj.config["owner"] :
-            commandCall(obj, params)
+            commandCall(obj, params, *args)
     return inner
 
 
 def user(commandCall) :
-    def inner(obj, params) :
+    def inner(obj, params, *args) :
         if params[1][0].user in obj.config["users"] or params[1][0].user == obj.config["owner"] :
-            commandCall(obj, params)
+            commandCall(obj, params, *args)
     return inner
