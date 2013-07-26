084da89594681c11e4ce34f64cdd2ed8e571ea1c
Owen Jones <owen@owenjones.net>
2013-07-26 07:03:05 +0100

    Bot can now recognise when the connection drops, aim to get back online and then tidy stuff up

--- mandelbot/network.py
+++ mandelbot/network.py
@@ -1,32 +1,47 @@
-# MANDELBOT
+# Mandelbot
 """
 IRC network model.
 Handles establishing a connection to, and interacting with, the IRC network.
 
 TODO:
 * Implement anti-flooding (buffer & time limit)
-* Separate basic commands from network model
+* Allow quiet mode to be applied on a per-channel basis
+* Work out better timeout metrics
 """
+import threading
 from mandelbot import utils, connection, channel
 from mandelbot.exceptions import *
 
 """
 Network States
+
+Lots of flags to keep track of what the bot is doing,
+and a few methods to alter groups of them
 """
 class state(object) :
     isConnected = False
-    isWelcome = False
+    isReconnecting = False
+    isTimingOut = False
+    isTimedOut = False
     isIdentified = False
     isQuiet = False
+    currentNickname = False
+
+    def reset(self) :
+        """Reset all the flags to their default values"""
+        self.isConnected = False
+        self.isReconnecting = False
+        self.isTimingOut = False
+        self.isTimedOut = False
+        self.isIdentified = False
+        self.isQuiet = False
+        self.currentNickname = False
 
     def connected(self) :
+        """We've connected to the network"""
         self.isConnected = True
-
-    def welcome(self) :
-        self.isWelcome = True
-
-    def identified(self) :
-        self.isIdentified = True
+        self.isTimedOut = False
+        self.isReconnecting = False
 
 class network(state) :
     bot = None
@@ -38,14 +53,18 @@ class network(state) :
     channels = {}
     config = {}
 
-    events = {"PING"    : "_pong",
-              "001"     : "_connected",
+    events = {"PING"    : "_ping",
               "JOIN"    : "_joined",
-              "NICK"    : "_nick",
-              "MODE"    : "_mode",
+              "MODE"    : "_modeChange",
+              "NICK"    : "_nickChange",
               "KICK"    : "_kicked",
               "NOTICE"  : "_message",
-              "PRIVMSG" : "_message"}
+              "PRIVMSG" : "_message",
+
+              "001"     : "_connected",
+              "433"     : "_nickInUse"}
+
+    timer = None
 
     def __init__(self, config, bot) :
         self.bot = bot
@@ -54,16 +73,16 @@ class network(state) :
     def event(self, call, params) :
         """Triggers the handler for the network event"""
         try :
-            call = self.events[call]
+            attr = self.events[call]
 
         except KeyError :
             return
 
         try :
-            getattr(self, call)(params)
+            getattr(self, attr)(params)
 
         except Exception as e :
-            utils.log().error("Error with builtin: [{}]".format(e))
+            utils.log().error("[{}][Builtin Error {}] {}".format(self.config["name"], call, e))
 
     def connect(self) :
         """Connects to the IRC network"""
@@ -83,15 +102,19 @@ class network(state) :
             self.nick()
 
         except InvalidConnectionInformation :
-            utils.log().error("Connection information for network \"{}\" is invalid.".format(self.config["name"]))
+            utils.log().error("[{}] Connection information is invalid.".format(self.config["name"]))
 
         except CouldNotConnect :
-            utils.log().error("Could not connect to network \"{}\".".format(self.config["name"]))
+            utils.log().error("[{}] Could not connect to network.".format(self.config["name"]))
+            if not self.isReconnecting :
+                self._reconnect()
 
     def parse(self, raw) :
         """Parses a received message from the network"""
+        self._resetTimeout()
+
         raw = raw.strip()
-        utils.log().debug("Received: {}".format(raw))
+        utils.log().debug("[{}] Received: {}".format(self.config["name"], raw))
         parts = raw.split(" ", 3)
         if parts[0] == "PING" :
             self.event("PING", parts[1])
@@ -106,7 +129,7 @@ class network(state) :
             addr = utils.host(addr)
 
             if tgt[0] != "#" :
-                tgt = self.config["nickname"] if cmd == "MODE" else addr.nick
+                tgt = tgt if cmd == "MODE" else addr.nick
 
             msg = msg[1:] if msg[0] is ":" else msg
 
@@ -114,7 +137,7 @@ class network(state) :
 
     def send(self, message) :
         """Sends a message to the network"""
-        utils.log().debug("Sent: {}".format(message))
+        utils.log().debug("[{}] Sent: {}".format(self.config["name"], message))
         message = message + self.delimiter
         self.connection.send(message)
 
@@ -143,6 +166,7 @@ class network(state) :
     def nick(self, nick = None) :
         """Changes the bots nickname on the network"""
         nick = nick if nick else self.config["nickname"]
+        self.currentNickname = nick
         self.send("NICK :{}".format(nick))
 
     def identify(self, params = None) :
@@ -155,12 +179,22 @@ class network(state) :
                                                           self.config["nickname"],
                                                           utils.password.decode(self.config["nickpass"])))
 
-            self.nick()
+    def ghost(self, params = None) :
+        """Mandelbot's nickname is already in use - get it back!"""
+        if self.config["nickpass"] :
+            self.send("PRIVMSG {} :ghost {} {}".format(self.config["nickserv"],
+                                                       self.config["nickname"],
+                                                       utils.password.decode(self.config["nickpass"])))
+
+        self.nick()
 
     # General Message Types
+    def ping(self) :
+        self.send("PING :{}".format(self.config["host"]))
+
     def message(self, target, message) :
         """Sends a message to a target on the network"""
-        if not self.isQuiet :
+        if (self.isQuiet and not target.startswith("#")) or not self.isQuiet :
             self.send("PRIVMSG {} :{}".format(target, message))
 
     def notice(self, target, message) :
@@ -172,7 +206,7 @@ class network(state) :
         self.message(params[1][2], message)
 
     # Network Events
-    def _pong(self, host) :
+    def _ping(self, host) :
         """PING"""
         self.send("PONG :{}".format(host[1:]))
 
@@ -189,43 +223,106 @@ class network(state) :
 
     def _connected(self, params) :
         """When the bot has successfully connected"""
-        utils.log().info("Connection established ({})".format(self.config["name"]))
-        self.identify()
+        utils.log().info("[{}] Connection established.".format(self.config["name"]))
         self.connected()
+        self.identify()
 
-    def _nick(self, params) :
-        """When a nickname is changed"""
-        if params[0].nick == self.config["nickname"] :
-            self.config["nickname"] = params[1]
+    def _identified(self, params) :
+        """We're now identified"""
+        utils.log().info("[{}] Identified.".format(self.config["name"]))
+        self.identified()
 
-    def _mode(self, params) :
+        # Required if nickname needed to be changed during a reconnect
+        if params[2] != self.config["nickname"] :
+            self.ghost()
+
+        self.joinChannels()
+
+    def _nickInUse(self, params) :
+        self.nick(self.config["nickname"] + "_")
+
+        if self.isIdentified :
+            self.ghost()
+
+    def _nickChange(self, params) :
+        if params[0] == self.currentNickname :
+            self.currentNickname == params[2]
+
+    def _modeChange(self, params) :
         """When a mode is changed"""
-        if params[2] == self.config["nickname"] :
+        target = params[2]
+        if target.startswith("#") :
+            #self.channels[target].modeChange(params)
+            pass
+
+        elif target == self.currentNickname :
             modes = params[3]
             action = "append" if modes[0] == "+" else "remove"
 
             for m in modes[1:] :
                 if m == "r" and action == "append" :
-                    utils.log().info("Identified on {}".format(self.config["name"]))
-                    self.identified()
-                    self.joinChannels()
+                    self._identified(params)
 
                 getattr(self.modes, action)(m)
 
     def _joined(self, params) :
         """When somebody joins a channel"""
-        if params[0].nick == self.config["nickname"] :
-            utils.log().info("{} joined {}".format(self.config["nickname"], params[1]))
+        if params[0].nick == self.currentNickname :
+            utils.log().info("[{}] Joined {}".format(self.config["name"], params[1]))
             self.channels[params[1].lower()].joined()
         else :
-            self.channels[params[1].lower()].users.append(params[2])
+            #self.channels[params[1].lower()].users[params[2].nick] =
+            pass
 
     def _kicked(self, params) :
         """When somebody is kicked from a channel"""
         kicked = params[3].split(" ", 1)
-        if kicked[0] == self.config["nickname"] :
-            utils.log().info("{} kicked from {} on {} ({})".format(self.config["nickname"],
+        if kicked[0] == self.currentNickname :
+            utils.log().info("[{}] Kicked from {} ({}).".format(self.config["name"],
                                                                 params[2],
-                                                                self.config["name"],
                                                                 kicked[1][1:]))
             self.channels[params[2].lower()].join()
+
+    # Timeout Handling
+    def _timer(self, time, call, *args) :
+        """Creates a new timer"""
+        if self.timer :
+            self.timer.cancel()
+            self.timer = None
+
+        self.timer = threading.Timer(time, call, args)
+        self.timer.start()
+
+    def _resetTimeout(self) :
+        """Resets the timer that checks for disconnects"""
+        self.isTimingOut = False
+        self.isTimedOut = False
+        self._timer(60, self._timeoutCheck)
+
+    def _timeoutCheck(self) :
+        """Occurs when the connection has been quiet for too long"""
+        if not self.isTimingOut :
+            self.isTimingOut = True
+            self.ping()
+            self._timer(30, self._timeout)
+
+    def _timeout(self) :
+        """Connection is unresponsive - attempt to reconnect"""
+        self.connection.thread.stop()
+        self.connection = None
+        self.reset()
+        self.isTimedOut = True
+        utils.log().warning("[{}] Connection timed out.".format(self.config["name"]))
+        self._reconnect()
+
+    def _reconnect(self, n = 0) :
+        self.isReconnecting = True
+
+        if n < 5 :
+            self._timer(60, self._reconnect, (n+1))
+            utils.log().warning("[{}] Attempting to reconnect ({} of 5)".format(self.config["name"], n+1))
+            self.connect()
+
+        else :
+            utils.log().warning("[{}] Could not reconnect. Will try again shortly.".format(self.config["name"]))
+            self._timer(300, self._reconnect)
